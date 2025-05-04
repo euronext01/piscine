7fa19adb7ca6020d8cd163fc67b5b6bef10317c7
jihadkharmiz <jihadkharmiz1@gmail.com>
2025-05-04 22:34:18 +0100

    Delete Quest2 directory

--- Quest2/ex.go
+++
@@ -1,7 +0,0 @@
-package main
-
-func main() {
-	for i := 'a'; i <= 'z'; i++ {
-		fmt.PrintRune(i)
-	}
-}
--- Quest2/go.mod
+++
@@ -1,5 +0,0 @@
-module Quest2
-
-go 1.23.6
-
-require github.com/01-edu/z01 v0.1.0 // indirect
